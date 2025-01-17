7e8b6419a88b89bbfe121c50f74226f243a64ba3
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-01-17 12:58:01 -0300

    feat: creating openProject and newProject methods

--- src/main/java/com/edit/image/Methods.java
+++ src/main/java/com/edit/image/Methods.java
@@ -1,15 +1,22 @@
 package com.edit.image;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -17,7 +24,7 @@ import javafx.stage.Stage;
 public class Methods {
 
 
-    public static void saveImage(Canvas canvas){
+    public void saveImage(Canvas canvas){
         FileChooser fileChooser = new FileChooser();
         SnapshotParameters sp = new SnapshotParameters();
         sp.setFill(Color.TRANSPARENT);
@@ -29,7 +36,7 @@ public class Methods {
     }
 
 
-    public static void quit(Stage stage, Canvas canvas){
+    public void quit(Stage stage, Canvas canvas){
             Alert alert = new Alert(AlertType.CONFIRMATION);
             alert.setTitle("Quit");
             alert.setHeaderText("Your changes were not saved");
@@ -39,4 +46,39 @@ public class Methods {
             }
             stage.close();
     }
+
+
+    public void newProject(){
+        try{
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/imageEdit.fxml"));
+            Scene scene = new Scene(root);
+            Canvas canvas = (Canvas) root.getChildrenUnmodifiable().getFirst();
+            stage.setScene(scene);
+            stage.setTitle("Editor");
+            stage.show();
+            stage.setOnCloseRequest(e -> quit(stage, canvas));
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void openProject(GraphicsContext gc) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Project");
+        fileChooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif")
+        );
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            try (FileInputStream inputStream = new FileInputStream(selectedFile)) {
+                Image image = new Image(inputStream);
+                gc.drawImage(image, 0, 0, image.getWidth(), image.getHeight());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } 
+    }
+
 }
